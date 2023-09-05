16201b7bcf7adfd14a1aa037234c2762ae475cc7
BalisongV <g0737752@mail.ru>
2023-09-05 11:27:41 +0300

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main{
-public static void main(String[] args){
-Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Array length: ");
+        int num = in.nextInt();
+        int sum = 0;
+        //float mean = 0.0f;
+
+        int[] array = new int[num];
+
+        for (int i = 0; i < array.length; i++){
+            System.out.printf("Array element %d: ", i);
+            array[i] = in.nextInt();
+            sum = sum + array[i];
+        }
+
+        float mean = sum / (float) num;
+
+        System.out.print("Your array:\n");
+        for (int j : array) {
+            System.out.println(j);
+        }
+
+        System.out.println(Arrays.toString(array));
+        System.out.printf("Sum: %d\nMean: %.2f\n", sum, mean);
 }}
